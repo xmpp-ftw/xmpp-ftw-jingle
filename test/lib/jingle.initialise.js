57e65452af8a20614711a7e4fb5dda0c8df71a8b
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-10-20 22:59:10 +0100

    More of the initialisation stanza handled

--- test/lib/jingle.initialise.js
+++ test/lib/jingle.initialise.js
@@ -18,6 +18,17 @@ describe('Jingle', function() {
             },
             makeCallback: function(error, data) {
                 this.callback(error, data)
+            },
+            fullJid: {
+                domain: 'montague.lit',
+                user: 'romeo',
+                resource: 'laptop'
+            },
+            getJidType: function(type) {
+                switch (type) {
+                    case 'full':
+                        return 'romeo@montague.lit/laptop'
+                }
             }
         }
         jingle = new Jingle()
@@ -75,6 +86,36 @@ describe('Jingle', function() {
                 request,
                 callback
             )
+        })
+
+        it('Sends expected stanza', function(done) {
+            var request = {
+                to: 'juliet@shakespeare.lit/balcony',
+                sessionId: '12345',
+                name: 'session-name-value'
+            } 
+            
+            xmpp.once('stanza', function(stanza) {
+                stanza.is('iq').should.be.true
+                stanza.attrs.id.should.exist
+                stanza.attrs.to.should.equal(request.to) 
+                stanza.attrs.type.should.equal('set')
+                var element = stanza.getChild('jingle', jingle.NS)
+                element.should.exist
+                element.attrs.action.should.equal('session-initiate')
+                element.attrs.sid.should.equal(request.sessionId)
+                element.attrs.initiator.should.equal(
+                   manager.fullJid.user + '@' + manager.fullJid.domain + '/' +
+                   manager.fullJid.resource
+                )
+                var content = element.getChild('content')
+                content.should.exist
+                content.attrs.creator.should.equal('initiator')
+                content.attrs.name.should.equal(request.name)
+  
+                done()
+            })
+            socket.emit('xmpp.jingle.initialise', request, function() {})
         })
         
     })
