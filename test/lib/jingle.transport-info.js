4d07764501d67d992bdb0bccc37d385721017084
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-10-22 17:42:46 +0100

    Basic code and tests for transport info

---
+++ test/lib/jingle.transport-info.js
@@ -0,0 +1,162 @@
+var should = require('should')
+  , Jingle = require('../../lib/jingle')
+  , ltx    = require('ltx')
+  , helper = require('../helper')
+
+describe('Jingle', function() {
+
+    var jingle, socket, xmpp, manager, request
+
+    before(function() {
+        request = require('../resources/json/transport-info.json')
+        socket = new helper.Eventer()
+        xmpp = new helper.Eventer()
+        manager = {
+            socket: socket,
+            client: xmpp,
+            trackId: function(id, callback) {
+                this.callback = callback
+            },
+            makeCallback: function(error, data) {
+                this.callback(error, data)
+            },
+            fullJid: {
+                domain: 'montague.lit',
+                user: 'romeo',
+                resource: 'laptop'
+            },
+            getJidType: function(type) {
+                switch (type) {
+                    case 'full':
+                        return 'romeo@montague.lit/laptop'
+                }
+            }
+        }
+        jingle = new Jingle()
+        jingle.init(manager)
+    })
+
+    describe('Transport Info', function() {
+      
+        it('Errors if no callback provided', function(done) {
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            socket.once('xmpp.error.client', function(error) {
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal('Missing callback')
+                error.request.should.eql({})
+                xmpp.removeAllListeners('stanza')
+                done()
+            })
+            socket.emit('xmpp.jingle.info', {})
+        })
+        
+        it('Errors if non-functional callback provided', function(done) {
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            socket.once('xmpp.error.client', function(error) {
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal('Missing callback')
+                error.request.should.eql({})
+                xmpp.removeAllListeners('stanza')
+                done()
+            })
+            socket.emit('xmpp.jingle.info', {}, true)
+        })
+            
+        it('Errors if no \'to\' key provided', function(done) {
+            var request = {}
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            var callback = function(error, success) {
+                should.not.exist(success)
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal('Missing \'to\' key')
+                error.request.should.eql(request)
+                xmpp.removeAllListeners('stanza')
+                done()
+            }
+            socket.emit(
+                'xmpp.jingle.info',
+                request,
+                callback
+            )
+        })
+
+        it('Errors if there\'s no \'jingle\' key', function(done) {
+            var request = { to: 'juliet@shakespeare.lit' }
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            var callback = function(error, success) {
+                should.not.exist(success)
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal('Missing \'jingle\' key')
+                error.request.should.eql(request)
+                xmpp.removeAllListeners('stanza')
+                done()
+            }
+            socket.emit(
+                'xmpp.jingle.info',
+                request,
+                callback
+            )
+        })
+
+        it('Errors if there\'s no \'sid\' key', function(done) {
+            var request = { to: 'juliet@shakespeare.lit', jingle: {} }
+            xmpp.once('stanza', function() {
+                done('Unexpected outgoing stanza')
+            })
+            var callback = function(error, success) {
+                should.not.exist(success)
+                error.type.should.equal('modify')
+                error.condition.should.equal('client-error')
+                error.description.should.equal('Missing \'sid\' key')
+                error.request.should.eql(request)
+                xmpp.removeAllListeners('stanza')
+                done()
+            }
+            socket.emit(
+                'xmpp.jingle.info',
+                request,
+                callback
+            )
+        })
+
+        /*it('Sends expected stanza', function(done) {
+            var request = {
+                to: 'juliet@shakespeare.lit/balcony',
+                jingle: {
+                  sid: '12345'
+                }
+            } 
+            
+            xmpp.once('stanza', function(stanza) {
+                stanza.is('iq').should.be.true
+                stanza.attrs.id.should.exist
+                stanza.attrs.to.should.equal(request.to) 
+                stanza.attrs.type.should.equal('set')
+                var element = stanza.getChild('jingle', jingle.NS)
+                element.should.exist
+                element.attrs.action.should.equal('session-initiate')
+                element.attrs.sid.should.equal(request.jingle.sid)
+                element.attrs.initiator.should.equal(
+                   manager.fullJid.user + '@' + manager.fullJid.domain + '/' +
+                   manager.fullJid.resource
+                )
+                done()
+            })
+            socket.emit('xmpp.jingle.info', request, function() {})
+        })*/
+
+    })
+
+})
